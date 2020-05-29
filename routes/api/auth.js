b32b87b07632d62a82601a96355ff4f779340cea
Dwayne Bynum <dbynum4597@gmail.com>
2020-05-28 21:58:59 -0400

    connecting frontend to back

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -10,7 +10,7 @@ const User = require('../../models/User');
 
 // @route    GET api/auth
 // @desc     Get user by token
-// @access   Private
+// @access   Private needs a token
 router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const router = require ('./routes/api/users');
 const app = express();
 
 const PORT = process.env.PORT||5000;
 connectDB();
-app.get('/',(req,res,next)=>{
-  res.send('hello world')
-})
+
+// init middleware
+//needed to parse json data req.body
+app.use(express.json({extended:false}));
+
+app.use('/api/users', require ('./routes/api/users'))
+app.use('/api/auth', require ('./routes/api/auth'))
+app.use('/api/profile', require ('./routes/api/profile'))
+app.use('/api/posts', require ('./routes/api/posts'))
 
 
 
